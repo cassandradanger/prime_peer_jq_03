2045762326bfea0dd06b873ac9375b081d1edf03
Casie Lynch <cds.lynch@gmail.com>
2015-04-27 17:59:21 -0500

    i don't even know what we did

--- script.js
+++ script.js
@@ -4,26 +4,34 @@ var apikey = 'ba7280b6a052562af59a3776f9607c43984e6b6a';
 // It is called after 'search' is executed.
 function searchCallback(results) {
 	for(var i = 0; i < results.length; i++){
-	$(".container").append(
-		"<div class = 'col-md-4 resultClick'><div class='button'><img src = " 
-		+ results[i].image.thumb_url + 
-		"></div><div class = 'title'> <strong>Name:</strong> " 
-		+ results[i].name + 
-		"</div><div class='extraInfo'><strong>Description:</strong> " 
-		+ results[i].deck + 
-		"<br><strong>Platform:</strong> " 
-		+ results[i].platforms[0].name + 
-		"<br><strong>Release Date:</strong> " 
-		+ results[i].original_release_date + 
-		"</div></div>");
+		
+			$(".container").children().next().append(
+			"<div class = 'col-md-4 resultClick'><div class='button'><img src = " 
+			+ results[i].image.icon_url + 
+			"></div><div class = 'title'> <strong>Name:</strong> " 
+			+ results[i].name + 
+			"</div><div class='extraInfo'><strong>Description:</strong> " 
+			+ results[i].deck + 
+			"<br><strong>Platform:</strong> " 
+			+ results[i].platforms[0].name + 
+			"<br><strong>Release Date:</strong> " 
+			+ results[i].original_release_date + 
+			"</div></div>");
+			if(i % 3 ==0){
+				$(".container").append("<div class='row'></div>")
+			}
 }
     console.log(results);
 }
 
 $(document).ready(function() {
 	// Start the search here!
-	$("")
-	search('batman');
+	$(".submit").on("click", function(){
+		$(".container").empty();
+		var gameSearch = $("#game_search").val();
+		$("#game_search").val('');
+		search(gameSearch);
+	});
 	
 	$(".container").on("click", ".resultClick", function(){
 
