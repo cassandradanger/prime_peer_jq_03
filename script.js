e98cf433a58fbe4dcc5315a7881954b9760d76fd
Casie Lynch <cds.lynch@gmail.com>
2015-04-27 15:30:58 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-var apikey = 'YOUR-API-KEY'; // Put your API key here
+var apikey = 'ba7280b6a052562af59a3776f9607c43984e6b6a';
 
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
+	for(var i = 0; i < results.length; i++){
+	$(".container").append("<div class = 'row'>Name: " + results[i].name + "<br/><img src = " + results[i].image.thumb_url + "><div class='extraInfo'>Description: " + results[i].deck + "<br>Platform: " + results[i].platforms[0].name + "<br>Release Date: " + results[i].original_release_date + "</div></div>");
+}
     console.log(results);
 }
 
 $(document).ready(function() {
-
+	
 	// Start the search here!
 	search('batman');
+	var count = 0;
+	$(".container").on("click", ".row", function(){
+		count++;
+		if(count == 1){
+		$(this).children().slideDown();
+			
+	} else {
+		$(".extraInfo").slideUp("slow");
+		count = 0;
+	}
+	});
 });
 
 // HELPER FUNCTION
