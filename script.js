42c34c22252b1f068db591dc0afbe684e39ba1f6
Casie Lynch <cds.lynch@gmail.com>
2015-04-27 16:59:30 -0500

    saving functionality from the destruction of CSS

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ var apikey = 'ba7280b6a052562af59a3776f9607c43984e6b6a';
 // It is called after 'search' is executed.
 function searchCallback(results) {
 	for(var i = 0; i < results.length; i++){
-	$(".container").append("<div class = 'row'>Name: " + results[i].name + "<br/><img src = " + results[i].image.thumb_url + "><div class='extraInfo'>Description: " + results[i].deck + "<br>Platform: " + results[i].platforms[0].name + "<br>Release Date: " + results[i].original_release_date + "</div></div>");
+	$(".container").append(
+		"<div class = 'col-md-4 resultClick'><div class='button'><img src = " 
+		+ results[i].image.thumb_url + 
+		"></div><div class = 'title'> <strong>Name:</strong> " 
+		+ results[i].name + 
+		"</div><div class='extraInfo'><strong>Description:</strong> " 
+		+ results[i].deck + 
+		"<br><strong>Platform:</strong> " 
+		+ results[i].platforms[0].name + 
+		"<br><strong>Release Date:</strong> " 
+		+ results[i].original_release_date + 
+		"</div></div>");
 }
     console.log(results);
 }
 
 $(document).ready(function() {
-	
 	// Start the search here!
+	$("")
 	search('batman');
-	var count = 0;
-	$(".container").on("click", ".row", function(){
-		count++;
-		if(count == 1){
-		$(this).children().slideDown();
-			
-	} else {
-		$(".extraInfo").slideUp("slow");
-		count = 0;
-	}
+	
+	$(".container").on("click", ".resultClick", function(){
+
+		$(this).children().last().slideToggle("slow");
+	
 	});
 });
 
